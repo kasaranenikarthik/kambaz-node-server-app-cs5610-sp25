f3f311652256fbb8af310b6681f9ea199d0a7a3d
karthikkgvs <kasaraneni.g@northeastern.edu>
2025-04-14 17:41:14 -0400

    A6

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -14,8 +14,14 @@ export default function UserRoutes(app) {
     res.json(status);
   };
 
-  const findAllUsers = async (req, res) => { ;
+  const findAllUsers = async (req, res) => { 
+    const currentUser = req.session["currentUser"];
+    console.log("Current User: ", currentUser);
     const { role, name } = req.query;
+    if (!currentUser) {
+      res.status(401).json({ message: "Unauthorized" });
+      return;
+    }
     if (role) {
       const users = await dao.findUsersByRole(role); 
       res.json(users); 
