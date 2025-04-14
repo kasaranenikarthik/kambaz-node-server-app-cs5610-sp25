06e4b9e6fd7ae271650e9916b4d69263d3d9785b
karthikkgvs <kasaraneni.g@northeastern.edu>
2025-04-14 16:36:59 -0400

    A6

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -68,7 +68,7 @@ export default function UserRoutes(app) {
       req.session["currentUser"] = currentUser;
       res.json(currentUser);
     } else {
-      res.status(401).jsoon({ message: "Unable to login. Try again later." });
+      res.status(401).json({ message: "Unable to login. Try again later." });
     }
   };
 
